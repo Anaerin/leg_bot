a20eccf3e4cff93d0154448d63478064633261a9
Robert Johnston <anaerin@gmail.com>
2016-03-12 11:29:35 -0600

    Initial attempt. Here's hoping it works...

--- client.js
+++ client.js
@@ -29,7 +29,9 @@ var ircClient = function () {
     this.clientConnection = new tmi.client(this.options);
     this.clientConnection.parent = this;
     this.whisperConnection = new tmi.client(this.whisper_options);
-    this.whisperConnection.parent = this;
+	this.whisperConnection.parent = this;
+	this.awsConnection = new tmi.client(this.aws_options);
+	this.awsConnection.parent = this;
     
     //Then we add listeners to those clients.
     this.clientConnection.on('chat', this.onChat);
@@ -40,9 +42,22 @@ var ircClient = function () {
     this.clientConnection.on('join', this.onChatJoin);
     this.clientConnection.on('part', this.onChatPart);
     this.clientConnection.on('notice', this.onChatNotice);
-    this.clientConnection.on('roomstate', this.onRoomState);
+	this.clientConnection.on('roomstate', this.onRoomState);
+	this.clientConnection.on('serverchange', this.onServerChange);
     this.clientConnection.connect();    
+	
+	this.awsConnection.on('chat', this.onChat);
+	this.awsConnection.on('mod', this.onMod);
+	this.awsConnection.on('unmod', this.onUnMod);
+	this.awsConnection.on('connected', this.onChatConnect);
+	this.awsConnection.on('disconnected', this.onChatDisconnect);
+	this.awsConnection.on('join', this.onChatJoin);
+	this.awsConnection.on('part', this.onChatPart);
+	this.awsConnection.on('notice', this.onChatNotice);
+	this.awsConnection.on('roomstate', this.onRoomState);
+	this.awsConnection.connect();
     
+
     this.whisperConnection.on('connected', this.onWhisperConnect);
     this.whisperConnection.on('disconnected', this.onWhisperDisconnect);
     this.whisperConnection.on('notice', this.onWhisperNotice);
@@ -182,11 +197,11 @@ c.updateLastSeen = function (user, channelID) {
 
 c.options = {
 	options: {
-        debug: false,
-        logger: log
+        debug: false
 	},
+	logger: log,
 	connection: {
-		random: 'chat',
+		cluster: 'main',
 		reconnect: true
 	},
 	identity: {
@@ -196,11 +211,11 @@ c.options = {
 };
 c.whisper_options = {
 	options: {
-        debug: false,
-        logger: log
+        debug: false
 	},
+	logger: log,
 	connection: {
-		random: 'group',
+		cluster: 'group',
 		reconnect: true
 	},
 	identity: {
@@ -208,6 +223,21 @@ c.whisper_options = {
 		password: token
 	}
 
+}
+c.aws_options = {
+	options: {
+		debug: false
+	},
+	logger: log,
+	connection: {
+		cluster: 'aws',
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: config.userName,
+		password: token
+	}
 }
 c.whisperCommands = [];
 
@@ -218,6 +248,8 @@ c.quitting = false;
 
 //We store Channel objects that we pass messages to
 c.channels = {};
+c.awsChannels = {};
+
 c.attachWhispers = function () {
     this.attachWhisper(commands.common);
     this.attachWhisper(commands.calendar);
@@ -258,7 +290,15 @@ c.whisper = function (user, channel, message) {
 }
 
 c.say = function (channel, message) {
-    client.clientConnection.say(channel, message);
+	var clientChannels = client.clientConnection.getChannels();
+	var awsChannels = client.awsConnection.getChannels();
+	if (clientChannels.indexOf(channel) >= 0) {
+		client.clientConnection.say(channel, message);
+	} else if (awsChannels.indexOf(channel) >= 0) {
+		client.awsConnection.say(channel, message);
+	} else {
+		log.error("Unable to say ", message, " to channel ", channel);
+	}
 }
 
 c.findChannel = function (username) {
@@ -302,12 +342,12 @@ c.replayWhisper = function(isError) {
         if (isError) {
             log.info("Whispering failed(?), saying to ", hashtag, ": ", displayname + ": " + client.lastWhisper.message);
             if (isFromWhisper) {
-                client.clientConnection.say(hashtag, displayname + " (whisper failed, replying to first common channel, are you following?): " + client.lastWhisper.message);
+                client.say(hashtag, displayname + " (whisper failed, replying to first common channel, are you following?): " + client.lastWhisper.message);
             } else {
-                client.clientConnection.say(hashtag, displayname + " (whisper failed, are you following?): " + client.lastWhisper.message);
+                client.say(hashtag, displayname + " (whisper failed, are you following?): " + client.lastWhisper.message);
             }
         } else {
-            client.clientConnection.say(hashtag, displayname + ": " + client.lastWhisper.message);
+            client.say(hashtag, displayname + ": " + client.lastWhisper.message);
         }
 	}
 }
@@ -338,6 +378,12 @@ c.partChannel = function (channel){
 	log.info('leaving', channel.hashtag);
 	delete channels[channel.hashtag];
     this.parent.clientConnection.part(channel.hashtag);
+}
+c.onServerChange = function (channel) {
+	var me = this.parent;
+	log.info("Switching to AWS for channel " + channel.hashtag);
+	me.clientConnection.part(channel.hashtag);
+	me.awsConnection.join(channel.hashtag);
 }
 
 var client = new ircClient();
